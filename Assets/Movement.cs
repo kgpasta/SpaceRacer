8c77e9c69b9b3966ccae95539d54990bec6ac571
salilgupta1 <salil.gupta323@gmail.com>
2015-02-08 20:26:11 -0600

    keyboard input

---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// move left
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Debug.Log ("Left Arrow has been hit");
+			rigidbody2D.AddForce(-Vector2.right*10);
+		}
+		// move right
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Debug.Log ("Right Arrow has been hit");
+			rigidbody2D.AddForce(Vector2.right);
+		}
+		// move forward
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			Debug.Log ("Forward Arrow has been hit");
+			rigidbody2D.AddForce(Vector2.up);		
+		}
+		// move backward
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			Debug.Log ("Backward Arrow has been hit");
+			rigidbody2D.AddForce(-Vector2.up);		
+		}
+	}
+//	void FixedUpdate(){
+//		rigidbody2D.AddForce (Vector2.);
+//	}
+
+}
