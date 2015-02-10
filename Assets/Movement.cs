b938600f7ee735f721d74c3730e930764c9662db
achalasani15 <chalasani.aashish@gmail.com>
2015-02-09 19:01:45 -0600

    fixed colliders

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,9 +7,16 @@ public class Movement : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+
+	// Stops rotation on collision 
+//	void OnCollisionEnter2D(Collider2D collision) {
+//		rigidbody2D.angularVelocity = 0;
+//		Debug.Log ("hi");
+//	}
+
 	// Update is called once per frame
 	void Update () {
+
 		// move left
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			Debug.Log ("Left Arrow has been hit");
@@ -23,12 +30,12 @@ public class Movement : MonoBehaviour {
 		// move forward
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			Debug.Log ("Forward Arrow has been hit");
-			rigidbody2D.AddForce(transform.up);		
+			rigidbody2D.AddForce(transform.up*100);		
 		}
 		// move backward
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			Debug.Log ("Backward Arrow has been hit");
-			rigidbody2D.AddForce(-transform.up,0);		
+			rigidbody2D.AddForce(-transform.up*100);		
 		}
 	}
 //	void FixedUpdate(){
