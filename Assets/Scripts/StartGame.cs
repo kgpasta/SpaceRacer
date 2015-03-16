1007b9ee1e2fcfcf6e1d57d217403ef1b6e05931
salilgupta1 <salil.gupta323@gmail.com>
2015-03-16 16:23:27 -0500

    new main menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+
+	public void Start(){
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GameStart(){
+		Application.LoadLevel ("Main");
+	}
+}
+
